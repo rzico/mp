ca15c0f44ea8beee0f38b2917320b40b3913441d
zhangsr <zhangsr@tiaohuo.com>
2017-09-09 12:09:30 +0800

    idea

---
+++ src/include/navbar.vue
@@ -0,0 +1,46 @@
+<template>
+    <header class="navbar" dataRole="navbar">
+        <text class="navbar-left" :style="{fontFamily:'iconfont'}" v-on:click="goback">&#xe669;</text>
+        <text class="navbar-title">{{title}}</text>
+    </header>
+</template>
+<style scoped>
+
+    .navbar {
+        background: #F0AD3C;
+        border-color: #707070;
+        height: 96px;
+        align-items:center;
+        flex-direction: row;
+    }
+    .navbar-left {
+        font-size: 48px;
+        width:96px;
+        align-items:center;
+    }
+    .navbar-title {
+        font-size: 48px;
+        text-align: left;
+        color: #707070;
+    }
+
+</style>
+<script>
+    export default {
+        props: {
+            title: { default: "navbar" }
+        },
+        created(){
+            var domModule=weex.requireModule("dom");
+            domModule.addRule('fontFace',{
+                'fontFamily':'iconfont',
+                'src':"url('http://cdn.rzico.com/weex/resources/fonts/iconfont.ttf')"
+            })
+        },
+        methods: {
+            goback: function (e) {
+                this.$emit('goback');
+            }
+        }
+     }
+</script>
---
+++ src/member/setup.vue
@@ -0,0 +1,143 @@
+<template>
+    <div class="wrapper">
+        <list class="list" style="margin: 0px;border: 0px;">
+            <navbar :title="title" @goback="goback"> </navbar>
+            <cell class="cell">
+                <div class="panel ">
+                    <image class="logo" resize="cover"
+                           src="https://img.alicdn.com/tps/TB1z.55OFXXXXcLXXXXXXXXXXXX-560-560.jpg">
+                    </image>
+                    <div style="flex-direction: column;">
+                        <text class="text name">张三</text>
+                        <text class="autograph">点击设置个性签名</text>
+                    </div>
+                </div>
+            </cell>
+            <cell class="cell">
+                <div class="panel">
+                    <text class="text" :style="{fontFamily:'iconfont'}" v-on:click="goback">&#xe669;</text>
+                    <text class="text name">通用设置</text>
+                    <text class="text" :style="{fontFamily:'iconfont'}" v-on:click="goback">&#xe669;</text>
+                </div>
+            </cell>
+            <cell class="cell">
+                <div class="panel">
+                    <text class="text" :style="{fontFamily:'iconfont'}" v-on:click="goback">&#xe669;</text>
+                    <text class="text name">专栏认证</text>
+                    <text class="text last" :style="{fontFamily:'iconfont'}" v-on:click="goback">&#xe669;</text>
+                </div>
+                <div class="panel clear">
+                    <text class="text" :style="{fontFamily:'iconfont'}" v-on:click="goback">&#xe669;</text>
+                    <text class="text name">商品管理</text>
+                    <text class="text last" :style="{fontFamily:'iconfont'}" v-on:click="goback">&#xe669;</text>
+                </div>
+                <div class="panel clear">
+                    <text class="text" :style="{fontFamily:'iconfont'}" v-on:click="goback">&#xe669;</text>
+                    <text class="text name">我的订单</text>
+                    <text class="text last" :style="{fontFamily:'iconfont'}" v-on:click="goback">&#xe669;</text>
+                </div>
+                <div class="panel clear">
+                    <text class="text" :style="{fontFamily:'iconfont'}" v-on:click="goback">&#xe669;</text>
+                    <text class="text name">评论管理</text>
+                    <text class="text last" :style="{fontFamily:'iconfont'}" v-on:click="goback">&#xe669;</text>
+                </div>
+                <div class="panel clear">
+                    <text class="text" :style="{fontFamily:'iconfont'}" v-on:click="goback">&#xe669;</text>
+                    <text class="text name">评论管理</text>
+                    <text class="text last" :style="{fontFamily:'iconfont'}" v-on:click="goback">&#xe669;</text>
+                </div>
+            </cell>
+            <cell class="cell">
+                <div class="panel">
+                    <text class="text" :style="{fontFamily:'iconfont'}" v-on:click="goback">&#xe669;</text>
+                    <text class="text name">推荐给朋友</text>
+                    <text class="text last" :style="{fontFamily:'iconfont'}" v-on:click="goback">&#xe669;</text>
+                </div>
+            </cell>
+            <cell class="cell">
+                <div class="panel clear">
+                    <text class="text" :style="{fontFamily:'iconfont'}" v-on:click="goback">&#xe669;</text>
+                    <text class="text name">关于美篇</text>
+                    <text class="text last" :style="{fontFamily:'iconfont'}" v-on:click="goback">&#xe669;</text>
+                </div>
+            </cell>
+        </list>
+    </div>
+
+</template>
+<style scoped>
+
+    .text {
+        font-size: 38px;
+    }
+
+    .wrapper {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        width: 750px;
+        background-color: #eeeeee;
+    }
+
+    .cell {
+        background-color: #eeeeee;
+    }
+
+    .panel {
+        align-items:center;
+        min-height: 100px;
+        flex-direction: row;
+        background-color: #ffffff;
+        margin: 20px 0px 20px 0px;
+        padding: 20px;
+        border-top:1px solid #ccc;
+        border-bottom:1px solid #ccc;
+    }
+    .clear {
+        margin: 0px 0px 00px 0px;
+        padding: 20px;
+        border-top:1px solid #ccc;
+        border-bottom:1px solid #ccc;
+    }
+
+    .logo {
+        width:150px;
+        height:150px;
+        margin:10px;
+        border-radius:50%;
+        overflow:hidden;
+    }
+
+    .autograph {
+        font-size: 28px;
+        text-align: left;
+        color: #ccc;
+    }
+
+    .last {
+
+    }
+</style>
+<script>
+    var navigator = weex.requireModule('navigator')
+    import navbar from '../include/navbar.vue'
+    export default {
+        components: {
+            navbar
+        },
+        props: {
+            title: { default: "设置" }
+        },
+        methods: {
+            goback: function (e) {
+                navigator.push({
+                    url: 'http://dotwe.org/raw/dist/519962541fcf6acd911986357ad9c2ed.js',
+                    animated: "true"
+                })
+            }
+        }
+
+    }
+</script>
