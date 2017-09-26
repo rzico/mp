7ae98d59b71f5d792484cff4f29cc39da4e62a16
guoshuzhi <admin@rzrcj.com.cn>
2017-09-26 23:10:24 +0800

    Afnetworking

---
+++ platforms/ios/Weex/Utils/Category/UIImage+Util.m
@@ -0,0 +1,154 @@
+//
+//  UIImage+Util.m
+//  iosapp
+//
+//  Created by ChanAetern on 2/13/15.
+//  Copyright (c) 2015 oschina. All rights reserved.
+//
+
+#import "UIImage+Util.h"
+
+@implementation UIImage (Util)
+
+// 同 - (UIImage *)jsq_imageMaskedWithColor:(UIColor *)maskColor
+
+- (UIImage *)imageMaskedWithColor:(UIColor *)maskColor
+{
+    NSParameterAssert(maskColor != nil);
+    
+    CGRect imageRect = CGRectMake(0.0f, 0.0f, self.size.width, self.size.height);
+    UIImage *newImage = nil;
+    
+    UIGraphicsBeginImageContextWithOptions(imageRect.size, NO, self.scale);
+    {
+        CGContextRef context = UIGraphicsGetCurrentContext();
+        
+        CGContextScaleCTM(context, 1.0f, -1.0f);
+        CGContextTranslateCTM(context, 0.0f, -(imageRect.size.height));
+        
+        CGContextClipToMask(context, imageRect, self.CGImage);
+        CGContextSetFillColorWithColor(context, maskColor.CGColor);
+        CGContextFillRect(context, imageRect);
+        
+        newImage = UIGraphicsGetImageFromCurrentImageContext();
+    }
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
+
+- (UIImage *)cropToRect:(CGRect)rect
+{
+    CGImageRef imageRef   = CGImageCreateWithImageInRect([self CGImage], rect);
+    UIImage *croppedImage = [UIImage imageWithCGImage:imageRef];
+    CGImageRelease(imageRef);
+    
+    return croppedImage;
+}
+
+
+- (UIImage *)circleImage {
+    
+    // 开始图形上下文
+    UIGraphicsBeginImageContextWithOptions(self.size, NO, 0.0);
+    
+    // 获得图形上下文
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    
+    // 设置一个范围
+    CGRect rect = CGRectMake(0, 0, self.size.width, self.size.height);
+    
+    // 根据一个rect创建一个椭圆
+    CGContextAddEllipseInRect(ctx, rect);
+    
+    // 裁剪
+    CGContextClip(ctx);
+    
+    // 将原照片画到图形上下文
+    [self drawInRect:rect];
+    
+    // 从上下文上获取剪裁后的照片
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    
+    // 关闭上下文
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
+
+
+/**
+ 按尺寸
+
+ @param image <#image description#>
+ @param size <#size description#>
+ @return <#return value description#>
+ */
+-(UIImage *)OriginImage:(UIImage *)image scaleToSize:(CGSize)size{
+    UIGraphicsBeginImageContext(size);
+    [image drawInRect:CGRectMake(0, 0, size.width, size.height)];
+    UIImage *scaledImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return scaledImage;
+}
+
+
+-(UIImage *) imageCompressForWidthScale:(UIImage *)sourceImage targetWidth:(CGFloat)defineWidth{
+    
+    UIImage *newImage = nil;
+    CGSize imageSize = sourceImage.size;
+    CGFloat width = imageSize.width;
+    CGFloat height = imageSize.height;
+    CGFloat targetWidth = defineWidth;
+    CGFloat targetHeight = height / (width / targetWidth);
+    CGSize size = CGSizeMake(targetWidth, targetHeight);
+    CGFloat scaleFactor = 0.0;
+    CGFloat scaledWidth = targetWidth;
+    CGFloat scaledHeight = targetHeight;
+    CGPoint thumbnailPoint = CGPointMake(0.0, 0.0);
+    
+    if(CGSizeEqualToSize(imageSize, size) == NO){
+        
+        CGFloat widthFactor = targetWidth / width;
+        CGFloat heightFactor = targetHeight / height;
+        
+        if(widthFactor > heightFactor){
+            scaleFactor = widthFactor;
+        }
+        else{
+            scaleFactor = heightFactor;
+        }
+        scaledWidth = width * scaleFactor;
+        scaledHeight = height * scaleFactor;
+        
+        if(widthFactor > heightFactor){
+            
+            thumbnailPoint.y = (targetHeight - scaledHeight) * 0.5;
+            
+        }else if(widthFactor < heightFactor){
+            
+            thumbnailPoint.x = (targetWidth - scaledWidth) * 0.5;
+        }
+    }
+    
+    UIGraphicsBeginImageContext(size);
+    
+    CGRect thumbnailRect = CGRectZero;
+    thumbnailRect.origin = thumbnailPoint;
+    thumbnailRect.size.width = scaledWidth;
+    thumbnailRect.size.height = scaledHeight;
+    
+    [sourceImage drawInRect:thumbnailRect];
+    
+    newImage = UIGraphicsGetImageFromCurrentImageContext();
+    
+    if(newImage == nil){
+        
+        //NSLog(@"scale image fail");
+    }
+    UIGraphicsEndImageContext();  
+    return newImage;  
+}
+@end
