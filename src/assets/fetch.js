1b373c3c8a10dab45d9742596f7e3641ca685990
zhangsr <zhangsr@tiaohuo.com>
2017-10-08 16:37:33 +0800

    idea

---
+++ src/assets/fetch.js
@@ -0,0 +1,28 @@
+const stream = weex.requireModule('stream')
+const baseURL = ''
+
+export function fetch (type,path,body) {
+  return new Promise((resolve, reject) => {
+    stream.fetch({
+      method:`${type}`,
+      url: `${baseURL}/${path}`,
+      type: 'json',
+      body:`${body}`
+    }, (response) => {
+      if (response.status == 200) {
+         resolve(response.data)
+      }
+      else {
+         reject(response)
+      }
+    }, () => {})
+  })
+}
+
+export function POST (path,body) {
+  return fetch('POST',path,body)
+}
+
+export function GET (path) {
+  return fetch('GET',path)
+}
