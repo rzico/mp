e456511af33f962f2dcb021e4600f21771b722cb
zhangsr <zhangsr@tiaohuo.com>
2017-10-17 21:42:10 +0800

    idea

--- src/assets/fetch.js
+++ src/assets/fetch.js
@@ -1,28 +1,37 @@
 const stream = weex.requireModule('stream')
 const baseURL = ''
 
-export function fetch (type,path,body) {
-  return new Promise((resolve, reject) => {
-    stream.fetch({
-      method:`${type}`,
-      url: `${baseURL}${path}`,
-      type: 'json',
-      body:`${body}`
-    }, (response) => {
-      if (response.status == 200) {
-         resolve(response.data)
-      }
-      else {
-         reject(response)
-      }
-    }, () => {})
-  })
-}
-
 export function POST (path,body) {
-  return fetch('POST',path,body)
+    return new Promise((resolve, reject) => {
+        stream.fetch({
+            method:'POST',
+            url: `${baseURL}${path}`,
+            type: 'json',
+            body:`${body}`
+        }, (response) => {
+            if (response.status == 200) {
+                resolve(response.data)
+            }
+            else {
+                reject(response)
+            }
+        }, () => {})
+    })
 }
 
-export function GET (path) {
-  return fetch('GET',path)
+export function GET (path,resolve,reject) {
+    stream.fetch({
+        method:'GET',
+        url: `${baseURL}${path}`,
+        type: 'json'
+    }, (response) => {
+        if (response.status == 200) {
+            if (response.data.type!="warn") {
+                resolve(response.data)
+            }
+        }
+        else {
+            reject(response)
+        }
+    }, () => {})
 }
