a005b2ff3b251cff269b01890d354b0128afe4f0
zhangsr <zhangsr@tiaohuo.com>
2017-11-01 10:47:21 +0800

    idea

--- src/assets/utils.js
+++ src/assets/utils.js
@@ -126,6 +126,7 @@ let utilsFunc = {
             return url;
         }
     },
+    //模糊图片，r , s  为 1-50，超大超模糊
     blur(url,r,s) {
         if (url.substring(0,10) == "http://cdn") {
             return url+"@"+r+"-"+s+"bl";
