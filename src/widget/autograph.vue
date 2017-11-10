6de49828e35995730e8d2e73444b73e8090f1fcc
xkwg <xkwg@163.com>
2017-11-10 14:13:55 +0800

    nzh

---
+++ src/widget/autograph.vue
@@ -0,0 +1,69 @@
+<template>
+    <div style="background-color: white">
+    <div class="header">
+        <navbar :title="title" :complete="complete" @goback="goback"  @goComplete="goComplete"> </navbar>
+    </div>
+    <div class="wrapper">
+        <textarea class="textarea" @input="oninput"   placeholder="不超过30个字" v-model="autograph"></textarea>
+    </div>
+    </div>
+</template>
+<style>
+    .textarea {
+        font-size: 32px;
+        width: 750px;
+        height: 750px;
+        padding-top: 20px;
+        padding-bottom: 20px;
+        padding-left: 20px;
+        padding-right: 20px;
+    }
+</style>
+<script>
+    import navbar from '../include/navbar.vue';
+    import utils from '../assets/utils'
+    const modal = weex.requireModule('modal');
+    const storage = weex.requireModule('storage');
+    var event = weex.requireModule('event');
+
+    export default {
+        data :function(){
+            return{
+                autograph:''
+            }
+        },
+        components: {
+            navbar
+        },
+        props: {
+            title: {default: "修改个性签名"},
+            complete: {default: "完成"}
+        },
+        created() {
+            var _this = this ;
+            var autotext = utils.getUrlParameter('name');
+            storage.getItem(autotext, e => {
+//                event.toast(e)
+                let textData = JSON.parse(e.data);
+                _this.autograph = textData.autograph;
+                storage.removeItem(autotext);
+            })
+        },
+        methods: {
+            oninput (event) {
+                console.log('oninput:', event.value);
+                this.text = event.value
+            },
+            back: function () {
+                event.closeURL()
+            },
+            goComplete:function (e) {
+                var E = {
+                  text:this.text
+                }
+                let backData = utils.message('success','成功',E);
+                event.closeURL(backData);
+            }
+        }
+    }
+</script>
