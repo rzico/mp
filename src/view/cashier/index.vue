5a8d397a9c7b7c2d7ba3e2cfcd05d7f38adfb0d1
zhangsr <zhangsr@tiaohuo.com>
2017-11-13 18:43:31 +0800

    idea

--- src/view/cashier/index.vue
+++ src/view/cashier/index.vue
@@ -1,61 +1,36 @@
 <template>
     <div class="wrapper">
-        <navbar :title="title" @goback="goback" :border="false"> </navbar>
+        <navbar :title="title" @goback="goback" :border="false" :complete="'账单'" @goComplete="deposit"> </navbar>
         <scroller class="scroller">
             <div class="wallet-panel">
-                <text class="wallet-title">钱包余额（元）</text>
                 <text class="balance">1000000.00</text>
-            </div>
-
-            <div class="cell-row cell-line">
-                <div class="cell-panel space-between" @click="bindingCard">
-                    <div class="flex-row flex-start">
-                        <text class="ico" :style="{fontFamily:'iconfont'}">&#xe64f;</text>
-                        <text class="title ml10">银行卡</text>
-                    </div>
-                    <div class="flex-row flex-end">
-                        <text class="sub_title">已绑定</text>
-                        <text class="arrow" :style="{fontFamily:'iconfont'}">&#xe630;</text>
-                    </div>
-                </div>
-
-                <div class="cell-panel space-between cell-clear" @click="cashCard">
-                    <div class="flex-row flex-start">
-                        <text class="ico" :style="{fontFamily:'iconfont'}">&#xe626;</text>
-                        <text class="title ml10">提现到银行卡</text>
-                    </div>
-                    <div class="flex-row flex-end">
-                        <text class="arrow" :style="{fontFamily:'iconfont'}">&#xe630;</text>
-                    </div>
+                <div class="wallet-title">
+                    <text class="sub_title">今天收款（元）</text>
+                    <text class="sub_title">昨天收款:29294</text>
                 </div>
             </div>
-            <div class="cell-row cell-line">
-                <div class="cell-panel space-between">
-                    <div class="flex-row flex-start">
-                        <text class="ico" :style="{fontFamily:'iconfont'}">&#xe61f;</text>
-                        <text class="title ml10">我的账单</text>
-                    </div>
-                    <div class="flex-row flex-end">
-                        <text class="arrow" :style="{fontFamily:'iconfont'}">&#xe630;</text>
-                    </div>
+            <div class="fontInput">
+                <text class="iconFont" :style="{fontFamily:'iconfont'}" >&#xe69f;</text>
+                <input class="input" type="number" placeholder="请输入消费金额" maxlength="7":autofocus="true" value="" @input="onmoney"  />
+            </div>
+            <div class="buttombox">
+                <div class="btn weixin">
+                    <text class="ico" :style="{fontFamily:'iconfont'}">&#xe659;</text>
+                    <text class="btn-text" value="微信钱包" @click="weixin()">微信钱包</text>
                 </div>
-                <div class="cell-panel space-between">
-                    <div class="flex-row flex-start">
-                        <text class="ico" :style="{fontFamily:'iconfont'}">&#xe698;</text>
-                        <text class="title ml10">我的赏金</text>
-                    </div>
-                    <div class="flex-row flex-end">
-                        <text class="arrow" :style="{fontFamily:'iconfont'}">&#xe630;</text>
-                    </div>
+                <div class="btn alipay">
+                    <text class="ico" :style="{fontFamily:'iconfont'}">&#xe659;</text>
+                    <text class="btn-text" value="支付宝" @click="weixin()">支付宝</text>
                 </div>
-                <div class="cell-panel space-between cell-clear">
-                    <div class="flex-row flex-start">
-                        <text class="ico" :style="{fontFamily:'iconfont'}">&#xe6ce;</text>
-                        <text class="title ml10">我的奖励</text>
-                    </div>
-                    <div class="flex-row flex-end">
-                        <text class="arrow" :style="{fontFamily:'iconfont'}">&#xe630;</text>
-                    </div>
+            </div>
+            <div class="buttombox">
+                <div class="btn weixin">
+                    <text class="ico" :style="{fontFamily:'iconfont'}">&#xe659;</text>
+                    <text class="btn-text" value="微信快捷登录" @click="weixin()">微信快捷登录</text>
+                </div>
+                <div class="btn alipay">
+                    <text class="ico" :style="{fontFamily:'iconfont'}">&#xe659;</text>
+                    <text class="btn-text" value="微信快捷登录" @click="weixin()">微信快捷登录</text>
                 </div>
             </div>
         </scroller>
@@ -66,28 +41,90 @@
 <style scoped>
 
     .wallet-title {
-        margin-top: 70px;
-        flex:1;
+        width: 620px;
+        margin-top: 20px;
         font-size: 32px;
         color: #fff;
         margin-left:60px;
-        border-bottom-width: ;
-        justify-content: center;
+        justify-content: space-between;
+        flex-direction: row;
+        align-items: center;
     }
 
     .wallet-panel {
         background-color:  #D9141E;
-        height:400px;
+        height:300px;
         flex-direction: column;
         align-items:flex-start;
     }
 
     .balance {
-        flex:3;
+        margin-top: 40px;
         font-size: 120px;
         color: #fff;
         margin-left:40px;
     }
+
+    .fontInput{
+        border-style: solid;
+        border-width:1px;
+        border-color:#CCC;
+        border-radius: 5px;
+        padding-left: 20px;
+        margin-top: 30px;
+        margin-left: 30px;
+        margin-right: 30px;
+        flex-direction: row;
+        align-items:center;
+        height: 120px;
+    }
+    .iconFont{
+        margin-top: 10px;
+        font-size: 80px;
+    }
+    .input{
+        width: 600px;
+        height: 100px;
+        font-size:50px;
+    }
+
+    .buttombox {
+        margin-top: 20px;
+        margin-left: 30px;
+        margin-right: 30px;
+        flex-direction: row;
+        width: 690px;
+    }
+
+    .btn {
+        margin-left: 20px;
+        margin-right: 20px;
+        margin-top: 10px;
+        flex-direction: row;
+        justify-content: center;
+        padding-top: 0px;
+        padding-top: 0px;
+        height: 80px;
+        flex:1;
+        align-items: center;
+        border-radius:12px;
+    }
+    .btn:active {
+        background-color:#ccc;
+    }
+    .weixin {
+        background-color:limegreen;
+        border-color:limegreen;
+    }
+    .alipay {
+        background-color:#0088fb;
+        border-color:#0088fb;
+    }
+    .card {
+        background-color:#0088fb;
+        border-color:#0088fb;
+    }
+
 
 </style>
 <script>
@@ -99,25 +136,13 @@
             navbar
         },
         props: {
-            title: { default: "钱包" }
+            title: { default: "收银台" }
         },
         methods: {
             goback: function (e) {
-//                navigator.pop({
-//                    url: 'http://cdn.rzico.com/weex/app/member/setup.js',
-//                    animated: "true"
-//                })
                 event.closeURL();
             },
-            cashCard:function (e) {
-                event.openURL('http://192.168.2.241:8081/withdrawal.weex.js', function () {
-                })
-            },
-            bindingCard:function (e) {
-                event.openURL('http://192.168.2.241:8081/bank.weex.js', function () {
-                })
-            },
-            setup: function (e) {
+            deposit:function () {
 
             }
         }
