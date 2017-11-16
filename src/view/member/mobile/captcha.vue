7f51ad20789e857c103a8c9783adcb2cb39fcaa2
xkwg <xkwg@163.com>
2017-11-16 15:55:54 +0800

    po

--- src/view/member/mobile/captcha.vue
+++ src/view/member/mobile/captcha.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="wrapper bkg-white">
         <navbar :title="title" @goback="goback"> </navbar>
-        <captcha :title="cptitle" @onclick="onclick"> </captcha>
+        <captcha :title="cptitle" @onclick="onclick" :mobile="mobile" @onSend="onSend" @onEnd="onEnd"> </captcha>
     </div>
 </template>
 <style lang="less" src="../../../style/wx.less"/>
@@ -14,7 +14,10 @@
     const modal = weex.requireModule('modal')
     var navigator = weex.requireModule('navigator')
     import navbar from '../../../include/navbar.vue'
+    import { POST, GET } from '../../../assets/fetch'
+    var event = weex.requireModule('event');
     import captcha from '../../../include/captcha.vue'
+    import utils from '../../../assets/utils';
     var stream = weex.requireModule('stream')
     export default {
         components: {
@@ -23,6 +26,65 @@
         props: {
             title: { default: "登录"},
             cptitle: { default: "输入验证码" }
+        },
+        data () {
+            return {
+                mobile:""
+            }
+        },
+        created() {
+            this.mobile = utils.getUrlParameter("mobile");
+//            this.onSend();
+            utils.debug(222)
+        },
+        methods: {
+            onSend: function (e) {
+                var _this = this;
+                event.encrypt(this.mobile,function (msg) {
+                    if (msg.type ==="success"){
+                        POST('weex/login/send_mobile.jhtml?mobile=' +msg.data).then(
+                            function (data) {
+                                if (data.type == "success") {
+                                    _this.$refs.captcha.beginTimer();
+                                } else {
+                                    _this.$refs.captcha.endTimer();
+                                    event.toast(data.content);
+                                }
+                            },
+                            function (err) {
+                                _this.$refs.captcha.endTimer();
+                                event.toast("网络不稳定")
+                            }
+                        )
+                    } else {
+                        event.toast(data.content);
+                    }
+                })
+            },
+            goback:function () {
+                event.closeURL()
+            },
+            onEnd: function (e) {
+                this.captcha = e;
+                event.encrypt(e,function (msg) {
+                    if (msg.type=="success") {
+                        POST('weex/login/captcha.jhtml?captcha=' + msg.data).
+                        then(function (data) {
+                                if (data.type == "success") {
+                                    let backData = utils.message('success','成功','');
+                                    event.closeURL(backData);
+                                } else {
+                                    event.toast(data.content);
+                                }
+                            },function () {
+                                event.toast("网络不稳定请重试");
+                            }
+                        )
+                    } else {
+                        event.toast(data.content);
+                    }
+                })
+            }
         }
     }
 </script>
