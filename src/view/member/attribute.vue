64b96d5577bd857233b6c377ddb98005407f69e1
张功洋 <1260438719@qq.com>
2017-11-25 11:09:11 +0800

    a

--- src/view/member/attribute.vue
+++ src/view/member/attribute.vue
@@ -431,7 +431,7 @@
                 var _this = this;
                 GET("weex/member/attribute.jhtml",
                     function (data) {
-                    utils.debug(data)
+//                    utils.debug(data)
                         if (data.type=="success") {
                             _this.attribute = data.data;
                             _this.updateStatus(_this.attribute);
