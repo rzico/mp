6df84298267057fe02495ebbf7a34224649ebadf
张功洋 <1260438719@qq.com>
2017-11-25 11:10:21 +0800

    a

--- src/view/shop/shop/storeList.vue
+++ src/view/shop/shop/storeList.vue
@@ -4,7 +4,8 @@
         <div class="head">
             <text class="clickAdd">+点击添加商铺</text>
         </div>
-        <list class="list">
+        <noData :noDataHint="noDataHint" v-if="isEmpty()"></noData>
+        <list class="list" v-if="isNoEmpty()">
             <refresh class="refresh" @refresh="onrefresh"  :display="refreshing ? 'show' : 'hide'">
                 <text class="indicator">下拉刷新</text>
             </refresh>
@@ -92,6 +93,7 @@ import navbar from '../../../include/navbar.vue';
 import {POST, GET} from '../../../assets/fetch';
 import utils from '../../../assets/utils';
 import filters from '../../../filters/filters';
+import noData from '../../../include/noData.vue';
 
 var event = weex.requireModule('event');
 const modal = weex.requireModule('modal');
@@ -105,10 +107,11 @@ export default {
         }
     },
     components: {
-        navbar
+        navbar,noData
     },
     props: {
         title: {default: "我的店铺"},
+        noDataHint: { default: "没有新朋友"},
     },
     created() {
         utils.initIconFont();
@@ -128,6 +131,12 @@ export default {
             }, function (err) {
                 event.toast(err.content)
             })
+        },
+        isNoEmpty:function() {
+            return this.lists.length!=0;
+        },
+        isEmpty:function() {
+            return this.lists.length==0;
         },
         //            上拉加载
         onloading (event) {
