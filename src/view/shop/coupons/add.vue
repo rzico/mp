cdec4adc676a98671b625484674253228ced3351
张功洋 <1260438719@qq.com>
2017-12-01 22:32:07 +0800

    a

--- src/view/shop/coupons/add.vue
+++ src/view/shop/coupons/add.vue
@@ -1,15 +1,13 @@
 <template>
     <div style="background-color: #eeeeee">
-        <div class="header">
             <navbar :title="title"  @goback="goback"  > </navbar>
-        </div>
         <div style="background-color: white">
     <div class="titleOne">
         <text class="titleText">填写优惠券信息:</text>
     </div>
     <div class="name">
-        <text class="nameText">名称</text>
-        <input type="text" placeholder="请输入优惠券名称" class="inputone" @change="" @input="oninput"/>
+        <text class="nameText">优惠券类型</text>
+        <input type="text" placeholder="请输入优惠券类型" class="inputone" @change="" @input="oninput"/>
     </div>
     <div class="scope">
         <text class="scopeText">适用范围</text>
@@ -18,29 +16,25 @@
     <div class="money">
         <text class="moneyText">满减面额</text>
         <input type="text" placeholder="请输入满减面额" class="input" @change="" @input="oninput"/>
-    </div>
-    <div class="discount">
-        <text class="discountText">折扣面额</text>
-        <input type="text" placeholder="请输入折扣面额" class="input" @change="" @input="oninput"/>
+        <text class="conditionsText">元</text>
     </div>
     <div class="conditions">
         <text class="conditionsText">使用条件</text>
         <text class="man" style="padding-left: 100px">满</text>
-        <input type="text" placeholder="" class="inputconditions" @change="" @input="oninput"/>
+        <input type="text" placeholder="0为无门槛" class="inputconditions" @change="" @input="oninput"/>
         <text class="conditionsText">元使用</text>
     </div>
-    <div class="time">
+    <div class="time" >
         <text class="textTime">使用时间</text>
-        <input type="text" placeholder="" class="inputtime" @change="" @input="oninput"/>
+        <div style="background-color: red" @click="Date">
+        <text class="begindate" >{{beginDate}}2222</text>
+        </div>
         <text class="textTime">一</text>
         <input type="text" placeholder="" class="inputtime" @change="" @input="oninput"/>
     </div>
-    <div class="titleTwo">
-        <text class="titleText">填写商家信息:</text>
-    </div>
-    <div class="shopName">
-        <text class="shopText">商家名称</text>
-        <input type="text" placeholder="请输入商家名称" class="input" @change="" @input="oninputeight"/>
+    <div class="introduced">
+        <text class="introducedText">规则介绍</text>
+        <input type="text" placeholder="使用规则介绍" class="input" @change="" @input="oninputeight"/>
     </div>
         </div>
     <div class="button">
@@ -64,6 +58,9 @@
     .titleText{
 
     }
+    .begindate{
+        width:300px;
+    }
     .name{
         flex-direction: row;
         align-items: center;
@@ -79,6 +76,12 @@
         font-size: 28px;
         height: 32px;
         width: 500px;
+    }
+    .inputone{
+        padding-left: 70px;
+        font-size: 28px;
+        height: 32px;
+        width: 300px;
     }
     .inputconditions{
         font-size: 28px;
@@ -137,18 +140,7 @@
         height: 100px;
         padding-left: 20px;
     }
-    .titleTwo{
-        height: 80px;
-        border-left-width: 5px;
-        flex-direction: row;
-        align-items: center;
-        background-color: white;
-        padding-left: 20px;
-        margin-left: 20px;
-        margin-top: 20px;
-        margin-bottom: 20px;
-    }
-    .shopName{
+    .introduced{
         flex-direction: row;
         align-items: center;
         border-top-width: 1px;
@@ -164,6 +156,7 @@
     }
 </style>
 <script>
+    const picker = weex.requireModule('picker');
     const storage = weex.requireModule('storage');
     var event = weex.requireModule('event');
     import navbar from '../../../include/navbar.vue';
@@ -171,7 +164,9 @@
     import { POST, GET } from '../../../assets/fetch'
     export default {
         data:function () {
-
+            return{
+                beginDate:'未设置',
+            }
         },
         components: {
             navbar
@@ -183,12 +178,23 @@
         created() {
             utils.initIconFont();
         },
-        methods:{
-            oninput:function (event){
+        methods: {
+            oninput: function (event) {
                 this.vendorName = event.value;
                 console.log('oninput', event.value);
             },
-        }
+            Date:function () {
+                utils.debug(111)
+                var _this = this;
+                picker.pickDate({
+                    value: _this.beginDate
+                }, event => {
+                    if (event.result === 'success') {
+                        _this.beginDate = event.data
+                    }
+                })
 
+            }
+        }
     }
 </script>
