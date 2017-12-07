cc2bed2a0bf216276cad8bfba5e4e416edd0331c
张功洋 <1260438719@qq.com>
2017-12-07 13:53:25 +0800

    a

--- src/view/shop/shop/storeList.vue
+++ src/view/shop/shop/storeList.vue
@@ -1,7 +1,28 @@
 <template>
     <div style="background-color: #eeeeee">
         <navbar :title="title" :complete="complete" @goback="goback"></navbar>
-        <div class="head" @click="add">
+        <div class="shopstwo">
+            <div class="deleteBoxTwo bkg-delete" @click="del()">
+                <text class="deleteText">删除</text>
+            </div>
+            <div  class="messageTwo"  @swipe="onpanmove($event,index)" @touchstart="onFriendtouchstart($event,index)">
+                <div class="shopLogo" >
+                    <image style="width: 250px;height: 200px;"  class="img" :src="logo"></image>
+                </div>
+                <div class="shopInformation">
+                    <div class="shopNameDiv">
+                        <text class="shopName">店铺名：</text>
+                        <text class="fullName">{{shopName}}</text>
+                    </div>
+                    <div class="shopAddressDiv">
+                        <text class="shopAddress">姓名：</text>
+                        <text class="concretely">{{name}}</text>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="head" @click="add" >
             <text class="clickAdd" >+点击添加商铺</text>
         </div>
         <noData :noDataHint="noDataHint" v-if="isEmpty()"></noData>
@@ -10,13 +31,13 @@
                 <text class="indicator">下拉刷新</text>
             </refresh>
             <cell :style="{minHeight:screenHeight + 'px'}">
-        <div class="shops" v-for="(num,index) in lists" v-if="!isOwner">
+        <div class="shops" v-for="(num,index) in lists">
             <div class="deleteBox bkg-delete" @click="del(num.id,index)">
                 <text class="deleteText">删除</text>
             </div>
             <div  class="message" @click="modification(num.id)" @swipe="onpanmove($event,index)" @touchstart="onFriendtouchstart($event,index)">
             <div class="shopLogo" >
-                <image style="width: 250px;height: 200px;"  class="img" :src="num.thedoor |thumbnail "></image>
+                <image style="width: 250px;height: 200px;"  class="img" :src="num.thedoor "></image>
             </div>
             <div class="shopInformation">
                 <div class="shopNameDiv">
@@ -30,32 +51,13 @@
             </div>
             </div>
         </div>
-                <div  class="shops" v-for="(num,index) in div" v-if="isOwner">
-                    <div class="deleteBox bkg-delete" @click="del()">
-                        <text class="deleteText">删除</text>
-                    </div>
-                    <div  class="message"  @swipe="onpanmove($event,index)" @touchstart="onFriendtouchstart($event,index)">
-                        <div class="shopLogo" >
-                            <image style="width: 250px;height: 200px;"  class="img" :src="num.logo |thumbnail "></image>
-                        </div>
-                        <div class="shopInformation">
-                            <div class="shopNameDiv">
-                                <text class="shopName">店铺名：</text>
-                                <text class="fullName">{{num.shopName}}</text>
-                            </div>
-                            <div class="shopAddressDiv">
-                                <text class="shopAddress">姓名：</text>
-                                <text class="concretely">{{num.name}}</text>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </cell>
             <loading class="loading" @loading="onloading" :display="loading ? 'show' : 'hide'">
                 <text class="indicator">加载中..</text>
             </loading>
         </list>
-    </div>
+        </div>
+
 </template>
 <style lang="less" src="../../../style/wx.less"/>
 <style>
@@ -65,11 +67,22 @@
         width: 750px;
         background-color: white;
     }
+    .messageTwo{
+        flex-direction: row;
+        align-items: center;
+        width: 730px;
+        border-radius: 10px;
+        background-color: white;
+    }
     .deleteText{
         font-size: 32px;color: #fff;
     }
     .deleteBox{
         position: absolute;right: 0px;top: 0px;height: 230px;align-items: center;width: 130px;justify-content: center;
+    }
+    .deleteBoxTwo{
+        position: absolute;right: 0px;top: 0px;height: 230px;align-items: center;width: 130px;justify-content: center;border-bottom-right-radius: 10px
+    ;border-top-right-radius: 10px;
     }
     .head {
         background-color: white;
@@ -90,7 +103,16 @@
         align-items: center;
         margin-bottom: 10px;
         height: 230px;
+    }
+    .shopstwo{
         background-color: white;
+        flex-direction: row;
+        align-items: center;
+        margin-top: 10px;
+        margin-right: 10px;
+        margin-left: 10px;
+        border-radius: 10px;
+        height: 230px;
     }
     .shopLogo{
         margin-left: 20px;
@@ -145,8 +167,11 @@ export default {
             refreshing : false,
             lists: [],
             screenHeight:0,
-            div:[],
-            isOwner:''
+            run:[],
+            isOwner:true,
+            name:'',
+            shopName:'',
+            logo:''
         }
     },
     components: {
@@ -163,8 +188,8 @@ export default {
         this.screenHeight = utils.fullScreen(136);
     },
     filters:{
-        thumbnail:function (value) {
-            return utils.thumbnail(value,250,200);
+        filterHead:function (value) {
+            return utils.filterHead(value,250,200);
         },
 
     },
@@ -181,6 +206,7 @@ export default {
         open:function () {
             let _this = this;
             GET('weex/member/shop/list.jhtml',function (mes) {
+                utils.debug(mes)
                 if (mes.type == 'success') {
                     _this.lists = mes.data.data
                 } else {
@@ -193,10 +219,12 @@ export default {
         openTwo:function () {
             let _this = this;
             GET('weex/member/enterprise/view.jhtml',function (mes) {
+                utils.debug(mes)
                 if (mes.type == 'success') {
                     _this.isOwner = mes.data.isOwner;
-                    _this.div = mes.data
-                    utils.debug(_this.div)
+                    _this.logo = mes.data.logo;
+                    _this.shopName =mes.data.shopName;
+                    _this.name = mes.data.name;
                 } else {
                     event.toast(res.content);
                 }
