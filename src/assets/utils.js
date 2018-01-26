c82736af775c405f4b70b36505cb8cf66bce304e
kezhijie <453471869@qq.com>
2018-01-26 19:47:14 +0800

    kzj

--- src/assets/utils.js
+++ src/assets/utils.js
@@ -3,9 +3,9 @@
  */
 const resLocateURL = 'file://';
 const resRemoteURL = 'http://cdn.rzico.com/weex/';
-const websiteURL = 'http://weixin.rzico.com';
+const websiteURL = 'http://dev.rzico.com';
 const event = weex.requireModule('event');
-const debug = false;//删掉该属性时请查找该页所有debug变量并删除变量
+const debug = true;//删掉该属性时请查找该页所有debug变量并删除变量
 let utilsFunc = {
     initIconFont () {
         let domModule = weex.requireModule('dom');
@@ -96,7 +96,7 @@ let utilsFunc = {
     //获取文章URL地址
     articleUrl(template,id) {
         template = template == '' ? 't1001' : template;
-        return websiteURL + "/" + template + "?id=" + id;
+        return websiteURL + "/#/" + template + "?id=" + id;
     },
     debug(msg) {
         if (debug) {
