416fd6282a23afbb9c5df93540cbef06d88ae524
张功洋 <1260438719@qq.com>
2018-01-30 11:05:16 +0800

    a

--- src/view/shop/goods/promote.vue
+++ src/view/shop/goods/promote.vue
@@ -5,12 +5,21 @@
             <text class="fz32 color999">开通分成推广,让推广者帮你推广商品,获得更多订单。</text>
             <text class="fz32 color999">推广者包含个人以及芸店合作渠道。</text>
             <text class="fz32 color999">你只需为因推广者而成功售出的商品付出佣金,交易不成功无需支付任何费用。</text>
-            <text class="fz32 colorRed mt30 bt30" @click="pickObject()">分成对象:  {{isobject}}</text>
-            <text class="fz32 color999">提示:设置完成后,请添加分销政策来设置佣金比例，系统支持返现金与积分方式。</text>
-            <text class="fz32 color999">通过本平台产生的店铺积分，1分=1元可在店铺内当现金消费。</text>
+            <div class="setting" @click="pickObject()">
+                <div class="flex-row">
+                    <text class="fz32 colorRed">分成对象:  {{isobject}}</text>
+                </div>
+                <div class="flex-row flex-end">
+                    <text class="arrow" :style="{fontFamily:'iconfont'}">&#xe630;</text>
+                </div>
+            </div>
         </div>
         <div class="completeButton" @click="complete()">
             <text class="fz40" style="color:white">完成</text>
+        </div>
+        <div class="warning">
+            <text class="fz32 color999">提示:设置完成后,请添加分销政策来设置佣金比例，系统支持返现金与积分方式。</text>
+            <text class="fz32 color999">通过本平台产生的店铺积分，1分=1元可在店铺内当现金消费。</text>
         </div>
     </div>
 </template>
@@ -34,6 +43,19 @@
         border-radius: 15px;
         margin: 20px;
     }
+    .warning{
+        padding: 30px;
+    }
+    .setting{
+        margin-top: 20px;
+        border-color:#eeeeee;
+        height: 80px;
+        border-top-width: 1px;
+        border-bottom-width: 1px;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+    }
     .color999{
         color:#999
     }
@@ -51,7 +73,7 @@
     export default {
         data: function () {
             return {
-                isobject:'点击设置',
+                isobject:'任何人',
                 begin:0,
                 PromoterType:''
             }
