1382fec81ead8b51af13c41155d2db0caa2e06b4
张功洋 <1260438719@qq.com>
2018-08-18 17:40:17 +0800

    a

--- src/view/shop/order/fill.vue
+++ src/view/shop/order/fill.vue
@@ -600,10 +600,11 @@
                                 var dataIndex = e.data;
                                 _this.begin = e.data;
                                 _this.paymentPluginId = _this.payId[dataIndex];
-                                _this.paymentPluginName = _this.payName[dataIndex]
+                                _this.paymentPluginName = _this.payName[dataIndex];
+                                _this.getmoneyTotal()
                             }
                         })
-                        _this.getmoneyTotal()
+
                     } else {
                         event.toast(mes.content);
                     }
