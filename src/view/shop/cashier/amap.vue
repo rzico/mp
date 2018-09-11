93422c01110175754397dae43028d3ae13d15bd9
张功洋 <1260438719@qq.com>
2018-09-11 10:52:55 +0800

    a

---
+++ src/view/shop/cashier/amap.vue
@@ -0,0 +1,18 @@
+<template>
+    <weex-amap class="map" id="map2017" :center="pos" ></weex-amap>
+</template>
+
+<script>
+    const amap = weex.requireModule('amap');
+    export default {
+        data(){
+            return {
+                pos: [116.487, 40.00003]
+            }
+        },
+
+        methods: {
+
+        }
+    }
+</script>
--- src/view/shop/cashier/index.vue
+++ src/view/shop/cashier/index.vue
@@ -173,10 +173,10 @@
                     <text class="ico_big" :style="{fontFamily:'iconfont'}">&#xe70e;</text>
                     <text class="menuBtn">员工</text>
                 </div>
-                <!--<div class="menu" @click="system()" >-->
-                    <!--<text class="ico_big" :style="{fontFamily:'iconfont'}">&#xe70e;</text>-->
-                    <!--<text class="menuBtn">系统</text>-->
-                <!--</div>-->
+                <div class="menu" @click="system()" >
+                    <text class="ico_big" :style="{fontFamily:'iconfont'}">&#xe70e;</text>
+                    <text class="menuBtn">地图</text>
+                </div>
                 <div class="content">
                     <text class="sub_title mt10">1.支持微信钱包、支付宝、店内会员卡、钱包</text>
                     <text class="sub_title mt10">2.单笔收钱金额不能超过5000元</text>
@@ -862,7 +862,7 @@
                 event.openURL(utils.locate("view/shop/admin/list.js"),function (e) {_this.clicked =false});
             },
             system(){
-                event.openURL(utils.locate("view/shop/cashier/pos/index.js"),function (e) {});
+                event.openURL(utils.locate("view/shop/cashier/amap.js"),function (e) {});
             },
             shop:function () {
                 if (this.clicked==true) {
