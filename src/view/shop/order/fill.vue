d4631ed9910039791807fc367f5215d85b50d8de
zhangsr <zhangsr@tiaohuo.com>
2019-01-20 21:03:27 +0800

    修改文字

--- src/view/shop/order/fill.vue
+++ src/view/shop/order/fill.vue
@@ -113,10 +113,10 @@
             </div>
             <div class="moneyBox">
                 <div class="flex-row space-between pb10" v-if="amountPayable>0 || arrears>0">
-                    <text class="moneyBoxText">应收金额: {{amountPayable}}元(其中上期欠款: {{arrears}}元)</text>
+                    <text class="moneyBoxText" style="color:red;">应收金额: {{amountPayable}}元(上期欠款: {{arrears}}元)</text>
                 </div>
                 <div class="flex-row space-between pb10" v-if="paperPayable>0 || ticket>0">
-                    <text class="moneyBoxText">应收水票: {{paperPayable}}张(其中上期欠票: {{ticket}}张)</text>
+                    <text class="moneyBoxText" style="color:red;">应收水票: {{paperPayable}}张(上期欠票: {{ticket}}张)</text>
                 </div>
             </div>
 
