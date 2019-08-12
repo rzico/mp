2df0235af3d74121b8990d56968be6cfbf88557c
张功洋 <1260438719@qq.com>
2019-08-12 15:32:43 +0800

    a

--- src/view/message/list.vue
+++ src/view/message/list.vue
@@ -541,6 +541,9 @@
                                         event.setReadMessage(item.userId,function(data){
                                             _this.clicked = false;
                                             if(data.type == 'success'){
+                                                _this.unReadMessage = event.getUnReadMessageCount();
+                                                var mesData = [0,0,_this.unReadMessage,0];
+                                                event.setDots(mesData);
                                                 event.navToChat(item.userId);
                                             }else{
                                                 event.toast(data.content);
