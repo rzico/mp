e352b63159f844089530a0d2a96594a6cb3c89a2
张功洋 <1260438719@qq.com>
2019-08-16 18:42:29 +0800

    a

--- src/view/shop/shipping/confirm.vue
+++ src/view/shop/shipping/confirm.vue
@@ -547,12 +547,12 @@
                             _this.arrears = 0;
                         }
 
-                        if (data.data.amountPaid>0) {
+                        if (Math.abs(data.data.amountPaid)>0) {
                             _this.amountPaid = '1';
                         } else {
                             _this.amountPaid = '0';
                         }
-                        if (data.data.paperPaid>0) {
+                        if (Math.abs(data.data.paperPaid)>0) {
                             _this.paperPaid = '1';
                         } else {
                             _this.paperPaid = '0';
