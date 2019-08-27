b307ef397c9b1385e71dcc5d144ab58142b1ae57
gongzeliang <447196008@qq.com>
2019-08-27 16:40:17 +0800

    gongzeliang

---
+++ src/view/shop/shop/chooseStamp.vue
@@ -0,0 +1,187 @@
+<template>
+    <div style="background-color: #eeeeee">
+        <navbar :title="title"  @goback="goback"  > </navbar>
+        <div class="bind">
+            <div class="iconfontDiv">
+                <text class="iconfont" :style="{fontFamily:'iconfont'}">&#xe64d;</text>
+            </div>
+            <text class="text">设置打印机</text>
+            <text class="sweepCode">{{u == '' ? '':'设备号:'+u}}</text>
+            <text class="sweepCodetwo">{{p == '' ? '':'设备密钥:'+p}}</text>
+        </div>
+        <div class="button bkg-primary button-bkg-img" @click="scan()">
+            <text class="buttonText">{{prompting}}</text>
+        </div>
+        <div class="skipBox">
+            <text class="skipMessage" @click="call('13860431130')">暂无打印机，购买热线 13860431130</text>
+        </div>
+    </div>
+</template>
+<style lang="less" src="../../../style/wx.less"/>
+<style>
+    .head{
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        background-color: white;
+        border-bottom-color: #cccccc;
+        border-bottom-width: 1px;
+        height: 120px;
+    }
+    .one{
+        font-size: 28px;
+        color:deepskyblue;
+    }
+    .two{
+        padding-left: 20px;
+        font-size: 28px;
+        color: deepskyblue;
+    }
+    .three{
+        padding-left: 20px;
+        font-size: 28px;
+        color:deepskyblue;
+    }
+    .four{
+        padding-left: 20px;
+        font-size: 28px;
+        color: #cccccc;
+    }
+    .bind{
+        flex-direction: column;
+        align-items: center;
+        background-color: white;
+    }
+    .iconfontDiv{
+        width: 150px;
+        height: 150px;
+        border-radius: 75px;
+        background-color:green;
+        align-items: center;
+        justify-content: center;
+        margin-top: 80px;
+    }
+    .text{
+        font-size: 32px;
+        margin-top: 40px;
+    }
+    .sweepCode{
+        font-size: 28px;
+        color: #999;
+        margin-top: 20px;
+        margin-bottom: 20px;
+    }
+    .sweepCodetwo{
+        font-size: 28px;
+        color: #999;
+        margin-bottom: 40px;
+    }
+    .iconfont{
+        color: white;
+        font-size: 100px;
+    }
+    .button{
+        margin-left:40px;
+        margin-right:40px;
+        height:82px;
+        align-items:center;
+        justify-content: center;
+        border-radius:15px;
+        position: absolute;
+        bottom:30px;
+        left: 0;
+        right: 0;
+    }
+    .buttonText{
+        font-size: 40px;
+        color: white;
+    }
+    .skipBox{
+        margin-top: 60px;
+        width: 750px;
+        align-items: center;
+        justify-content: center;
+    }
+    .skipMessage{
+        font-size: 32px;
+        color: #0088fb;
+    }
+</style>
+<script>
+    var event = weex.requireModule('event');
+    import navbar from '../../../include/navbar.vue';
+    import utils from '../../../assets/utils';
+    import { POST, GET, SCAN } from '../../../assets/fetch';
+    const phone = weex.requireModule('phone');
+
+    export default {
+        data: function () {
+            return{
+                prompting:'扫码绑定',
+                shopId:'',
+                code:'',
+                clicked:false,
+                u:'',
+                p:''
+            }
+        },
+        components: {
+            navbar
+        },
+        props: {
+            title: {default: "修改打印机"},
+
+        },
+        created() {
+            utils.initIconFont();
+            this.shopId = utils.getUrlParameter('shopId');
+            if(!utils.isNull(utils.getUrlParameter('u'))){
+                this.u = utils.getUrlParameter('u');
+                this.prompting = '重新绑定';
+            }
+            if(!utils.isNull(utils.getUrlParameter('p'))){
+                this.p = utils.getUrlParameter('p');
+            }
+        },
+        methods:{
+            goback:function () {
+                event.closeURL()
+            },
+            call(e){
+                phone.tel(e,function(){
+                    return;
+                })
+            },
+            scan:function() {
+                if (this.clicked==true) {
+                    return;
+                }
+                this.clicked = true;
+                var _this=this
+                event.scan(function (message) {
+                    _this.clicked =false
+                    if (message.type=='error') {
+                        _this.clicked =false
+                        return;
+                    }
+                    _this.u = utils.getUrlParameter('m',message.data);
+                    _this.p = utils.getUrlParameter('p',message.data);
+                    POST('weex/member/shop/printer.jhtml?shopId='+_this.shopId+'&p='+_this.p + '&u='+_this.u).then(
+                        function (mes) {
+                            event.toast(mes)
+                            if (mes.type == "success") {
+                                    _this.clicked =false
+                                let message = utils.message('success','成功');
+                                    event.closeURL(message);
+                            } else {
+                                event.toast(mes.content);
+                            }
+                        }, function (err) {
+                            event.toast("网络不稳定");
+                        }
+                    )
+                });
+            }
+        }
+    }
+</script>
---
+++ src/view/shop/shop/shopChoose.vue
@@ -0,0 +1,433 @@
+<template>
+    <div style="background-color: #eeeeee">
+        <navbar :title="title"  @goback="goback"  > </navbar>
+        <scroller>
+            <div class="appellation">
+                <text class="vendorName">商家名称</text>
+                <div style="flex-direction: row;align-items: center">
+                    <input type="text" placeholder="请输入商家名称" class="input" v-model="vendorName" />
+                    <text class="fontsIcon" :style="{fontFamily:'iconfont'}">&#xe630;</text>
+                </div>
+            </div>
+            <div class="industry" @click="industry">
+                <div class="left">
+                    <text class="belongIndustry">所属行业</text>
+                </div>
+                <div class="right">
+                    <text class="industryName">{{industryName}}</text>
+                    <text class="fontsIcon" :style="{fontFamily:'iconfont'}">&#xe630;</text>
+                </div>
+            </div>
+            <div class="location" @click="location">
+                <div class="left">
+                    <text class="businessLocation">所在地区</text>
+                </div>
+                <div class="right">
+                    <text class="generalLocation">{{addressName}}</text>
+                    <text class="fontsIcon" :style="{fontFamily:'iconfont'}">&#xe630;</text>
+                </div>
+            </div>
+            <div class="appellation"  >
+                <text class="detailedAddress">详细地址</text>
+                <div class="flex-row">
+                    <input type="text" placeholder="请输入详细地址" class="addressInput"  v-model="detailedAddress"/>
+                    <text class="fontsIcon" :style="{fontFamily:'iconfont'}">&#xe630;</text>
+                </div>
+            </div>
+            <div class="appellation">
+                <text class="contactName">联系姓名</text>
+                <div style="flex-direction: row;align-items: center">
+                    <input type="text" placeholder="请输入法人姓名" class="input" v-model="contactName" @change="" />
+                    <text class="fontsIcon" :style="{fontFamily:'iconfont'}">&#xe630;</text>
+                </div>
+            </div>
+            <div class="appellation">
+                <text class="contactNumber">联系电话</text>
+                <div style="flex-direction: row;align-items: center">
+                    <input type="number" placeholder="请输入法人电话"  maxlength="11" class="input" v-model="contactNumber" />
+                    <text class="fontsIcon" :style="{fontFamily:'iconfont'}">&#xe630;</text>
+                </div>
+            </div>
+            <div style="height: 750px"></div>
+        </scroller>
+        <div class="button bkg-primary button-bkg-img" @click="goComplete">
+            <text class="buttonText">完成</text>
+        </div>
+
+    </div>
+</template>
+<style lang="less" src="../../../style/wx.less"/>
+<style>
+    .changeGpsBox{
+        flex-direction: row;
+        align-items: center;
+        justify-content:space-between;
+        border-bottom-width: 1px;
+        border-bottom-color: #cccccc;
+        background-color: white;
+        height: 200px;
+        padding-left: 20px;
+        padding-right: 20px;
+    }
+    .changeAddress{
+        width: 500px;
+        font-size: 30px;
+        color: #999;
+    }
+    .gpsBox{
+        align-items: center;
+        justify-content:center;
+        border-bottom-width: 1px;
+        border-bottom-color: #cccccc;
+        background-color: white;
+        height: 200px;
+    }
+    .gpsIcon{
+        font-size: 80px;
+        color: #999;
+    }
+    .changeBox{
+        flex-direction: column;
+        align-items: center;
+    }
+    .changeIcon{
+        font-size: 60px;
+        color: #999;
+    }
+    .changeText{
+        font-size: 28px;
+        color: #999;
+    }
+    .head{
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        background-color: white;
+        border-bottom-color: #cccccc;
+        border-bottom-width: 1px;
+        height: 120px;
+    }
+    .one{
+        font-size: 28px;
+        color:deepskyblue;
+    }
+    .two{
+        padding-left: 20px;
+        font-size: 28px;
+        color: #cccccc;
+    }
+    .three{
+        padding-left: 20px;
+        font-size: 28px;
+        color: #cccccc;
+    }
+    .four{
+        padding-left: 20px;
+        font-size: 28px;
+        color: #cccccc;
+    }
+    .right{
+        flex-direction: row;
+        align-items: center;
+    }
+    /*商家名称*/
+    .appellation{
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+        border-bottom-width: 1px;
+        border-bottom-color: #cccccc;
+        background-color: white;
+        height: 100px;
+        padding-left: 20px;
+        padding-right: 20px;
+    }
+    .vendorName{
+        font-size: 32px;
+    }
+    .fontsIcon{
+        font-size: 28px;
+        color: #999;
+    }
+    /*所属行业*/
+    .industry{
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        background-color: white;
+        border-bottom-color: #ccc;
+        border-bottom-width: 1px;
+        height: 100px;
+        padding-left: 20px;
+        padding-right: 20px;
+    }
+    .belongIndustry{
+        font-size: 32px;
+    }
+    .industryName{
+        font-size: 32px;
+        color: #999;
+    }
+    /*商家区位*/
+    .location{
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        background-color: white;
+        border-bottom-color: #ccc;
+        border-bottom-width: 1px;
+        height: 100px;
+        padding-left: 20px;
+        padding-right: 20px;
+    }
+    .businessLocation{
+        font-size: 32px;
+    }
+    .generalLocation{
+        font-size: 32px;
+        color: #999;
+    }
+    /*详细地址*/
+    .address{
+        flex-direction: row;
+        align-items: center;
+        background-color: white;
+        border-bottom-color: #ccc;
+        border-bottom-width: 1px;
+        height: 100px;
+        padding-left: 20px;
+        padding-right: 20px;
+    }
+    .detailedAddress{
+        font-size: 32px;
+        line-height: 32px;
+    }
+    /*联系姓名*/
+    .name{
+        flex-direction: row;
+        align-items: center;
+        border-bottom-width: 1px;
+        border-bottom-color: #cccccc;
+        background-color: white;
+        margin-top: 50px;
+        height: 100px;
+        padding-left: 20px;
+        padding-right: 20px;
+    }
+    .contactName{
+        font-size: 32px;
+    }
+    .contactNumber{
+        font-size: 32px;
+    }
+    .input{
+        padding-left: 100px;
+        font-size: 32px;
+        line-height: 32px;
+        text-align: right;
+        height: 100px;
+        width: 500px;
+        color: #999;
+    }
+    .addressInput{
+        text-align: right;
+        font-size: 32px;
+        line-height: 32px;
+        width: 500px;
+        height: 100px;
+        color: #999;
+    }
+    /*联系电话*/
+    .call{
+        flex-direction: row;
+        align-items: center;
+        border-bottom-width: 1px;
+        border-bottom-color: #cccccc;
+        background-color: white;
+        height: 100px;
+        padding-left: 20px;
+        padding-right: 20px;
+    }
+    /*下一步*/
+    .button{
+        margin-left:40px;
+        margin-right:40px;
+        height:82px;
+        align-items:center;
+        justify-content: center;
+        border-radius:15px;
+        position: absolute;
+        bottom:30px;
+        left: 0;
+        right: 0;
+    }
+    .buttonText{
+        font-size: 40px;
+        color: white;
+    }
+</style>
+<script>
+    const storage = weex.requireModule('storage');
+    var event = weex.requireModule('event');
+    import navbar from '../../../include/navbar.vue';
+    import utils from '../../../assets/utils';
+    import { POST, GET } from '../../../assets/fetch'
+
+    export default {
+        data: function () {
+            return{
+//              商家名称
+                vendorName:'',
+//              联系人
+                contactName:'',
+//              联系电话
+                contactNumber:'',
+//              地址
+                detailedAddress:'',
+//              店铺id
+                shopId:'',
+//              营业执照
+                licensePhoto:'',
+//              场所照片
+                palcePhoto:'',
+//              门面照
+                logo:'',
+//              区位id
+                areaId:'',
+//              =========================================
+//              具体地址
+                addressName:'',
+                hasChange:false,
+                category:1,
+                industryName:'',
+                clicked:false,
+//              经纬度
+                lng:'',
+                lat:''
+
+            }
+        },
+        components: {
+            navbar
+        },
+        props: {
+            title: {default: "修改店铺"},
+
+        },
+        created() {
+            utils.initIconFont();
+            this.shopId = utils.getUrlParameter('shopId');
+            if(utils.isNull(this.shopId)) {
+                this.shopId = ''
+            }else {
+                if(this.shopId == '#'){
+                    this.shopId = ''
+                }
+                this.hasChange = true;
+                this.modification();
+            }
+        },
+        methods:{
+            modification:function () {
+                var _this = this;
+                GET('weex/member/shop/view.jhtml?shopId='+_this.shopId,function (mes) {
+                    if (mes.type == 'success') {
+                        _this.shopId = mes.data.id;
+                        _this.addressName = mes.data.areaName;
+                        _this.licensePhoto = mes.data.license;
+                        _this.logo = mes.data.thedoor;
+                        _this.areaId = mes.data.areaId;
+                        _this.palcePhoto = mes.data.scene;
+                        _this.vendorName = mes.data.name;
+                        _this.detailedAddress = mes.data.address;
+                        _this.contactName = mes.data.linkman;
+                        _this.contactNumber = mes.data.telephone
+                        _this.category = mes.data.categoryId
+                        _this.industryName = mes.data.categoryName
+
+                    } else {
+                        event.toast(mes.content);
+                    }
+                }, function (err) {
+                    event.toast(err.content)
+                })
+            },
+            goback:function () {
+                event.closeURL()
+            },
+            industry:function () {
+                var _this = this;
+                event.openURL(utils.locate('widget/list.js?listId=' + this.category + '&type=category'), function (data) {
+                    if(data.type == 'success' ) {
+                        _this.category = parseInt(data.data.listId);
+                        _this.industryName = data.data.listName;
+                    }
+                })
+            },
+            location:function () {
+                var _this = this;
+                event.openURL(utils.locate('view/shop/amap-picker/amap-picker.js?title='+encodeURIComponent('选择地址')+'&isPolygon=false'), function (data) {
+                    if(data.type == 'success' && data.data !='' ) {
+                        _this.addressName = data.data.areaName;
+                        _this.detailedAddress = data.data.building;
+                        _this.areaId = data.data.areaId;
+                        _this.lat = data.data.latitude;
+                        _this.lng = data.data.longitude;
+                    }
+                })
+            },
+            goComplete:function () {
+                if (this.clicked==true) {
+                    return;
+                }
+                this.clicked = true;
+                var _this=this;
+                if(utils.isNull(_this.vendorName)){
+                    event.toast('商家名称未填写');
+                    _this.clicked =false
+                    return
+                }else if(utils.isNull(_this.industryName)){
+                    event.toast('所属行业未选择');
+                    _this.clicked =false
+                    return
+                }else if(utils.isNull(_this.addressName)){
+                    event.toast('商家区位未选择');
+                    _this.clicked =false
+                    return
+                }else if(utils.isNull(_this.detailedAddress)){
+                    event.toast('商家地址未填写');
+                    _this.clicked =false
+                    return
+                }else if(utils.isNull(_this.contactName)){
+                    event.toast('法人姓名未填写');
+                    _this.clicked =false
+                    return
+                }else if(utils.isNull(_this.contactNumber)){
+                    event.toast('法人电话未填写');
+                    _this.clicked =false
+                    return
+                }else if(!/^[0-9]+$/.test(_this.contactNumber)){
+                    event.toast('请填写正确法人电话');
+                    _this.clicked =false
+                    return
+                }
+                POST('weex/member/shop/submit.jhtml?id='+this.shopId +'&name=' +encodeURI(this.vendorName)+'&areaId='+this.areaId+'&address=' +encodeURI(this.detailedAddress)+'&license=' +this.licensePhoto+
+                    '&scene=' +this.palcePhoto+'&thedoor=' +this.logo+'&linkman=' +encodeURI(this.contactName)+'&telephone=' +this.contactNumber+'&categoryId='+this.category+"&lng=" + _this.lng + "&lat=" +_this.lat).then(
+                    function (mes) {
+                        _this.clicked =false
+                        if (mes.type == "success") {
+                            event.toast(mes.content)
+                            let message = utils.message('success','成功');
+                            event.closeURL(message)
+                        } else {
+                            event.toast(mes.content);
+                        }
+                    }, function (err) {
+                        event.toast("网络不稳定");
+                        _this.clicked =false
+                    }
+                )
+            }
+        }
+    }
+</script>
